1b2cc676b760050175641b1a3c2576abe9ab8d41
EungSeon <31647768+EungSeon@users.noreply.github.com>
2017-09-19 16:47:51 +0900

    project#4

--- project#3.py
+++ project#3.py
@@ -1,19 +1,36 @@
-f = open("data_input.txt", "r")
-r = f.read()
+import urllib.request
 
-words = r.split()       # 쪼개진 단어들을 배열로 words에 저장
+url = "http://news.joins.com/article/21947878"
+
+f = urllib.request.urlopen(url)
+data = f.read().decode('utf-8')         # utf-8 또는 euc-kr로 decode
+
+begin = data.find("꽃게철이다.")
+end = data.find("시사점을 준다.")
+
+end += len("시사점을 준다.")
+print("total =", len(data))
+print("begin =", begin)
+print("end =", end)
+print(data[begin:begin+100])
+print(data[end-100:end])
+print("length =", end-begin)
+
+speech = data[begin:end]
+speech = speech.replace("<br/>", "")
+speech = speech.replace("&nbsp", "")
+speech = speech.split()
 
 # sort
 
 analyze = {}
-for i in words:
+for i in speech:
     analyze[i] = analyze.get(i, 0) + 1
     # get함수는 key값(i)가 dictionary안에 있을 경우 value값 출력, 없으면 뒤의 값(여기서는 0)출력
 
 lst = sorted(analyze.items(), key=lambda kv:kv[1], reverse=True)
 # items 함수는 dictionary의 key와 value를 모두 출력한다.
 # 여기서 reverse = True이면 내림차순, False면 오름차순
-# lambda는 뭘까?
 
 print("number of words is ", len(lst))
 # print(lst)
@@ -25,3 +42,4 @@ for k, v in lst:
     if cnt > 400:
         break
     cnt += 1
+
